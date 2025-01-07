645f7eff798781a9c1d57b80dbb4c5b20cba6a66
bhanuprakash1226 <bhanuprakash@tilicho.in>
2025-01-07 21:13:33 +0530

    The message mail updated to `info@sribharathi.com` in "to"

--- email-templates/contact-form.php
+++ email-templates/contact-form.php
@@ -5,7 +5,7 @@ if( ! empty( $_POST['email'] ) ) {
 	$enable_smtp = 'no'; // yes OR no
 
 	// Email Receiver Address
-	$receiver_email = 'info@domain.com';
+	$receiver_email = 'info@sribharathi.com';
 
 	// Email Receiver Name for SMTP Email
 	$receiver_name 	= 'Your Name';
